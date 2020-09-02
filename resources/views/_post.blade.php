e61460543ac631cca343205331e70da8c460343c
SauleBoi <biggsauliz@gmail.com>
2020-09-02 23:48:33 +0200

    Layout

---
+++ resources/views/_post.blade.php
@@ -0,0 +1,16 @@
+<div class="flex p-3 border border-b-gray-400">
+  <div class="mr-1 flex-shrink-0">
+    <img
+        src="/images/profile-pic-test.png"
+        alt=""
+        class="rounded-full mr-2"
+    >
+  </div>
+
+  <div>
+    <h5 class="font-bold mb-4">John Doe</h5>
+    <p class="text-sm">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a augue tincidunt, ultrices nibh ac, interdum nisi. Praesent aliquet volutpat velit, at venenatis odio feugiat nec. In vitae lectus id arcu dictum facilisis. Cras pulvinar sollicitudin ante ac fringilla. Vivamus mollis mauris in venenatis porttitor.
+    </p>
+  </div>
+</div>
---
+++ resources/views/_publish-post-panel.blade.php
@@ -0,0 +1,20 @@
+<div class="border border-orange-400 rounded-lg px-8 py-6 mb-8">
+  <form action="">
+    <textarea
+      name="body"
+      class="w-full"
+      placeholder="Write here..."
+    ></textarea>
+
+    <hr class="my-4">
+
+    <footer class="flex justify-between">
+      <img
+          src="/images/profile-pic-test.png"
+          alt=""
+          class="rounded-full mr-2"
+      >
+      <button type="submit" class="bg-orange-500 rounded-full shadow py-2 px-4 text-white">Submit</button>
+    </footer>
+
+</div>
